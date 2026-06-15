618584fd02f5b01cac090862f5add93777f8bbda
mikh-maksi <newlevelcenter@gmail.com>
2026-06-15 19:21:01 +0300

    check2

--- src/utils/wayforpayRedirectTest.ts
+++ src/utils/wayforpayRedirectTest.ts
@@ -3,8 +3,8 @@ import { WayForPayRequest } from '../../types/wayforpay';
 export function redirectToWayForPay(data: WayForPayRequest): void {
   const form = document.createElement('form');
   form.method = 'POST';
-  // form.action = 'https://secure.wayforpay.com/pay';
-  form.action = 'https://manec.science.kh.ua/wayforpay/';
+  form.action = 'https://secure.wayforpay.com/pay';
+  // form.action = 'https://manec.science.kh.ua/wayforpay/';
   form.acceptCharset = 'utf-8';
   // отримаємо дані з WayForPayRequest
   Object.entries(data).forEach(([key, value]) => {
@@ -12,17 +12,17 @@ export function redirectToWayForPay(data: WayForPayRequest): void {
     if (Array.isArray(value)) {
       value.forEach((item) => {
         const input = document.createElement('input');
-        // input.type = 'hidden';
+        input.type = 'hidden';
         input.name = `${key}[]`; // ← ПОВЕРТАЄМО []
         input.value = String(item);
         form.appendChild(input);
         
-        // const input1 = document.createElement('input');
-        // // input.type = 'hidden';
-        // input1.name = `${key}[]`; // ← ПОВЕРТАЄМО []
-        // if (["productName", "productCount"].includes(key)){input1.value = String(item);}
-        // if (["productPrice"].includes(key)){input1.value = "0";}        
-        // form.appendChild(input1);
+        const input1 = document.createElement('input');
+        input.type = 'hidden';
+        input1.name = `${key}[]`; // ← ПОВЕРТАЄМО []
+        if (["productName", "productCount"].includes(key)){input1.value = String(item);}
+        if (["productPrice"].includes(key)){input1.value = "0";}        
+        form.appendChild(input1);
 
       });
     } else {
