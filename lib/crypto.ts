e2bb19c5c3389c5fca7fdcdde97093217dc7bc3c
mikh-maksi <newlevelcenter@gmail.com>
2026-06-15 20:00:20 +0300

    check6_checkcrypto

--- lib/crypto.ts
+++ lib/crypto.ts
@@ -5,8 +5,8 @@ export const createSignature = (
   values: (string | number)[],
 ) => {
   const dataString = values.map((v) => String(v ?? '')).join(';');
-  console.log(dataString);
-  console.log(secret);
+  console.log("datastring = ", dataString);
+  console.log("Secret= ",secret);
   const hash = crypto
     .createHmac('md5', secret)
     .update(dataString, 'utf8')
